db661fbfc139d29cd644af965f20b6ab78674774
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2026-06-22 18:13:04 -0700

    backend

---
+++ public/scripts/global.js
@@ -0,0 +1,109 @@
+var mouseX = 0, mouseY = 0;
+var xOffset = 0, yOffset = 0;
+var bgOffset = 0.0;
+
+function init() {
+    initStyle();
+    applyClasses();
+    buildPage();
+    addListeners();
+    requestAnimationFrame(frame);
+}
+
+function initStyle() {
+    // Initialize the values of certain css properties
+    document.documentElement.style.setProperty("--hue", (Math.random() * 360) + "deg");
+    document.documentElement.style.setProperty("--bg-offset", "0px");
+}
+
+function applyClasses() {
+    // Make most visible elements draggable
+    const draggable = document.querySelectorAll("h1,h2,h3,img:not(.undraggable),div:not(.undraggable)");
+    draggable.forEach(element => {
+        element.classList.add("draggable");
+    });
+
+    // Make most visible elements scale up upon hover
+    const hoverable = document.querySelectorAll(".draggable,a");
+    hoverable.forEach(element => {
+        element.classList.add("hoverable");
+    });
+
+    const links = document.querySelectorAll("a");
+    links.forEach(element => {
+        if (element.href.indexOf("https") != -1) {
+            element.target = "_blank";
+        }
+    });
+}
+
+function buildPage() {
+    // Add background
+    const bg = document.createElement("div");
+    bg.classList.add("bg", "undraggable");
+    document.body.insertBefore(bg, document.body.childNodes[0]);
+
+    // Add home button
+    if (!document.body.classList.contains("no-home-button")) {
+        const home = document.createElement("a");
+        home.classList.add("metal", "home", "button", "hoverable", "rainbow");
+        home.draggable = false;
+        home.href = "index.html";
+        home.textContent = "Home";
+        document.body.appendChild(home);
+    }
+}
+
+function addListeners() {
+    // window.addEventListener('mousedown', onMouseDown);
+    // window.addEventListener('mouseup', onMouseUp);
+    // window.addEventListener('mousemove', onMouseMove);
+}
+
+function onMouseDown(event) {
+    if (event.target.classList.contains("hoverable")) {
+        event.target.classList.add("selected");
+        if (event.target.classList.contains("draggable")) {
+            event.target.classList.add("dragged");
+            
+            rect = event.target.getBoundingClientRect();
+            window.xOffset = rect.left - window.mouseX;
+            window.yOffset = rect.top - window.mouseY;
+
+            event.target.style.position = "fixed";
+            event.target.style.left = window.mouseX + "px";
+            event.target.style.top = window.mouseY + "px";
+
+            event.target.remove();
+            document.body.appendChild(event.target);
+        }
+    }
+}
+
+function onMouseUp(event) {
+    for (element of document.getElementsByClassName("selected")) {
+        element.classList.remove("selected");
+        element.classList.remove("dragged");
+    }
+}
+
+function onMouseMove(event) {
+    window.mouseX = event.clientX;
+    window.mouseY = event.clientY;
+}
+
+function frame(timestamp) {
+    // Animate background
+    window.bgOffset += 0.5;
+    document.documentElement.style.setProperty("--bg-offset", Math.floor(window.bgOffset) + "px");
+    
+    // Move dragged element to mouse
+    for (element of document.getElementsByClassName("dragged")) {
+        element.style.left = window.xOffset + window.mouseX + "px";
+        element.style.top = window.yOffset + window.mouseY + "px";
+    }
+    
+    requestAnimationFrame(frame);
+}
+
+init();
--- src/index.js
+++ src/index.js
@@ -8,16 +8,93 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import testimonialsData from './data/testimonials.json';
+import linksData from './data/links.json';
+import projectsData from './data/projects.json';
+
+const formatter = new Intl.NumberFormat('en', { notation: 'compact'});
+
+const projectsCacheLifespan = 20 * 1000;
+var projectsCache;
+var cacheTime = -1;
+
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
 		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
+			case '/api/testimonial': {
+				const currentIndex = parseInt(url.searchParams.get('currentIndex')) ?? -1;
+
+				let nextIndex;
+				if (currentIndex == -1) {
+					nextIndex = Math.floor(Math.random() * testimonialsData.length);
+				} else {
+					nextIndex = currentIndex == testimonialsData.length - 1 ? 0 : currentIndex + 1;
+				}
+
+				return Response.json({ ...testimonialsData[nextIndex], index: nextIndex });
+			}
+			case '/api/links': {
+				return Response.json(linksData);
+			}
+			case '/api/projects': {
+				if (cacheTime == -1 || Date.now() - cacheTime > projectsCacheLifespan) {
+					cacheTime = Date.now();
+					
+					projectsCache = [];
+					let downloadCounts = await getDownloadCounts(env);
+					for (let i = 0; i < projectsData.length; i++) {
+						projectsCache.push({ ...projectsData[i], downloads: downloadCounts[i] });
+					}
+				}
+
+				return Response.json(projectsCache);
+			}
 			default:
 				return new Response('Not Found', { status: 404 });
 		}
 	},
 };
+
+async function getDownloadCounts(env) {
+	try {
+		const curseforgeIds = projectsData.map(project => project.curseforge).filter(project => project != null);
+		const modrinthIds = projectsData.map(project => project.modrinth).filter(project => project != null);
+
+		const curseforgeData = await fetch('https://api.curseforge.com/v1/mods', {
+			method: 'POST',
+			body: `{"modIds": ${JSON.stringify(curseforgeIds)}}`,
+			headers: {
+				'Content-Type':'application/json',
+				'Accept':'application/json',
+				'x-api-key':env.CURSEFORGE_API_KEY
+			}
+		})
+		.then(response => response.json());
+
+		const modrinthData = await fetch(`https://api.modrinth.com/v2/projects?ids=${JSON.stringify(modrinthIds)}`)
+			.then(response => response.json());
+		modrinthData.sort((a, b) => modrinthIds.indexOf(a.id) - modrinthIds.indexOf(b.id));
+
+		let downloadCounts = [];
+		for (const obj of projectsData) {
+			let downloads = 0;
+			
+			let curseforgeIndex = curseforgeIds.indexOf(obj.curseforge);
+			if (curseforgeIndex != -1) {
+				downloads += curseforgeData.data[curseforgeIndex].downloadCount;
+			}
+
+			let modrinthIndex = modrinthIds.indexOf(obj.modrinth);
+			if (modrinthIndex != -1) {
+				downloads += modrinthData[modrinthIndex].downloads;
+			}
+
+			downloadCounts.push(formatter.format(downloads));
+		}
+
+		return downloadCounts;
+	} catch (e) {
+		console.error("An error occurred whilst requesting download counts:", e);
+	}
+}
